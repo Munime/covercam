0d81b568e78894f172f08dc4f6294d1aa364062a
Andrew-AJ <stopmotion.aj@gmail.com>
2021-01-06 12:45:58 +0200

    broke homepage into components and styling menuitems

---
+++ src/pages/homepage/homepage.js
@@ -0,0 +1,12 @@
+import "./homepage.styles.scss";
+import DirectoryMenu from "../../components/directory-menu/directoryMenu";
+
+const HomePage = () => {
+  return (
+    <div className="homepage">
+      <DirectoryMenu />
+    </div>
+  );
+};
+
+export default HomePage;
