aade41ef56cd67020e777c22f06ce5f34e31c30b
Andrew-AJ <stopmotion.aj@gmail.com>
2021-01-06 15:19:51 +0200

    added navigation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage";
 
 function App() {
   return (
-    <div>
-      <HomePage />
-    </div>
+    <Switch>
+      <Route exact path="/" component={HomePage} />
+    </Switch>
   );
 }
 
