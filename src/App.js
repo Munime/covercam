25cb06b7d453f27e9ed6107b68431206590913d0
Andrew-AJ <stopmotion.aj@gmail.com>
2021-01-08 14:28:35 +0200

    added logo, created Header component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage";
 import ShopPage from "./pages/shop-page/shoppage";
+import Header from "./components/header/header";
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/" component={HomePage} />
-      <Route path="/shop" component={ShopPage} />
-    </Switch>
+    <>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
+    </>
   );
 }
 
