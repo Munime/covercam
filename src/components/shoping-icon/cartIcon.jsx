5cede18362691e4e635d27a78b1a7557f9460b49
Andrew-AJ <stopmotion.aj@gmail.com>
2021-01-14 16:47:28 +0200

    added reselect, created selctors for cartItems and cartItemsCount and put it into mapStateToProps

--- src/components/shoping-icon/cartIcon.jsx
+++ src/components/shoping-icon/cartIcon.jsx
@@ -2,17 +2,22 @@ import React from "react";
 import { connect } from "react-redux";
 import ShoppingIcon from "../../assets/shopingIcon";
 import { toggleCartHidden } from "../../redux/cart/cart.actions";
+import { selectCartItemsCount } from "../../redux/cart/cart.selectors";
 import "./cart-icon.styles.scss";
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemsCount }) => (
   <div className="cart-icon" onClick={toggleCartHidden}>
     <ShoppingIcon className="shopping-icon" />
-    <span className="item-count">0</span>
+    <span className="item-count">{itemsCount}</span>
   </div>
 );
 
 const mapDispatchToProps = (dispatch) => ({
   toggleCartHidden: () => dispatch(toggleCartHidden()),
 });
+// з допомогою функц reduce, ми виводимо в itemCounts  кількість доданих айтемів
+const mapStateToProps = (state) => ({
+  itemsCount: selectCartItemsCount(state),
+});
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,26 @@
+import { createSelector } from "reselect";
+
+// функція, яка бере зі стейту, тільки його кусок.
+// в даному випадку частину cart
+
+const selectCart = (state) => state.cart; // input selector
+
+// ця функція бере проперті з того куска стейту, який ми виділили
+export const selectCartItems = createSelector(
+  // вона приймає в себе два аргументи.
+  // перший це масив, виділеного нами куска стейту (input selector)
+  [selectCart],
+  // другий - це функція, яка поверне нам значення, яке нам потрібно з input selector
+  (cart) => cart.cartItems
+);
+
+// селектимо частину стейту, i робим мемоізовану функцію, яка виводить загальну пораховану кількість айтемів в корзині
+export const selectCartItemsCount = createSelector(
+  [selectCartItems],
+  (cartItems) =>
+    cartItems.reduce(
+      (accumulatedQuantity, cartItem) =>
+        accumulatedQuantity + cartItem.quantity,
+      0
+    )
+);
